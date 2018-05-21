1de349b69137daf816cd76121e4681a56d00d328
Aymen Dhaouadi <aymen_dhaouadi@yahoo.fr>
2018-05-21 13:16:24 +0100

    V2

---
+++ TUI.Files.Service/Strategy/XmlFileReaderStrategy.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TUI.Files.Service.Models;
+
+namespace TUI.Files.Service.Strategy
+{
+    public class XmlFileReaderStrategy : IFileReaderStrategy
+    {
+        public IEnumerable<FileData> ReadFile(string directoryPath)
+        {
+            DirectoryInfo directoryInfo = new DirectoryInfo(directoryPath);
+            List<FileData> fileDataCollection = new List<FileData>();
+            foreach (FileInfo file in directoryInfo.EnumerateFiles("*.xml", SearchOption.AllDirectories))
+            {
+                fileDataCollection.Add(new FileData
+                {
+                    Name = file.Name,
+                    Content = File.ReadAllText(file.FullName)
+                });
+            }
+
+            return fileDataCollection;
+        }
+    }
+}
